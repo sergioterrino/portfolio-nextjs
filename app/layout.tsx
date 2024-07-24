1f54f6b55824662d9b1f795cf0eda168415dfa53
Sergio Terrino <120554805+sergioterrino@users.noreply.github.com>
2024-07-24 13:51:44 +0200

    17 commit - modify the head on layout.tsx for include the tag name

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,7 @@ import type { Metadata } from "next";
 import { Urbanist } from "next/font/google";
 
 import "./globals.css";
-
-//importamos swiperJs
-import 'swiper/css';
+import 'swiper/css'; //importamos swiperJs
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
@@ -33,15 +31,16 @@ export default function RootLayout({
       <Head>
         <title>Sergio Terrino - Portfolio Web</title>
         <meta name="description" content="Portfolio Web made by Sergio Terrino - Desarrollador y Programador Web y Móvil" />
-        <meta property="og:title" content="Sergio Terrino - Portfolio Web" />
-        <meta property="og:description" content="Portfolio Web made by Sergio Terrino - Desarrollador y Programador Web y Móvil" />
+        <meta name="title" property="og:title" content="Sergio Terrino - Portfolio Web" />
+        <meta name="description" property="og:description" content="Portfolio Web made by Sergio Terrino - Desarrollador y Programador Web y Móvil" />
         <meta property="og:url" content="https://stb-portfolio.netlify.app/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://github.com/user-attachments/assets/7a0738ee-e4aa-4b4c-bf2d-7cc0bde1ccd1" />
+        <meta name="image" property="og:image" content="https://github.com/user-attachments/assets/7a0738ee-e4aa-4b4c-bf2d-7cc0bde1ccd1" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Tu Portfolio" />
-        <meta name="twitter:description" content="App & Mobile Developer" />
+        <meta name="twitter:title" content="Sergio Terrino - Portfolio Web" />
+        <meta name="twitter:description" content="Portfolio Web made by Sergio Terrino - Desarrollador y Programador Web y Móvil" />
         <meta name="twitter:image" content="https://github.com/user-attachments/assets/7a0738ee-e4aa-4b4c-bf2d-7cc0bde1ccd1" />
+        <meta name="author" content="[Sergio Terrino Brea"></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className={urbanist.className}>
